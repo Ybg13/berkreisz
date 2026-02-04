263bfe0ee15332ac838ec79396c3b823ffd53a7e
Ybg13 <berkbey187@gmail.com>
2026-02-04 19:10:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -150,3 +150,10 @@ const AegisJS = (function() {
 
 // Yazılımı Tek Komutla Başlat
 AegisJS.start();
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-list');
+
+menu.addEventListener('click', function() {
+  menuLinks.classList.toggle('active'); // 'active' class'ını ekler veya çıkarır
+});
